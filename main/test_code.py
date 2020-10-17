1a2a0d85e7f5d80cd2787ebb9fb029a29c0060e3
Atena Darvishi <atenadarvishi@Atenas-MacBook-Pro.local>
2020-10-17 19:01:42 -0400

    adding the first function

--- main/test_code.py
+++ main/test_code.py
@@ -1,1 +1,16 @@
 print ("test code for python")
+
+
+
+
+def myfunction(number):
+    print("here we are executing some fansy function")
+    for i in range(0,number,2):
+        print(i)
+    return
+
+
+
+a=15
+myfunction(a)
+print("your request completed")
